a9b7b729f2358e21e1d1d325ed18214597c2bc24
aya1151 <105955636+aya1151@users.noreply.github.com>
2025-04-22 18:41:05 -0400

    Update mlp.py

--- mlp.py
+++ mlp.py
@@ -2,8 +2,8 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.datasets import make_classification
 from sklearn.model_selection import train_test_split
 
-X, y = make_classification(n_samples=100, random_state=1)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+#X, y = make_classification(n_samples=100, random_state=1)
+#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # might need to play around with the hidden layer and max_iter hyperparameter to get better results
 clf = MLPClassifier(hidden_layer_sizes=(128,), activation='relu', max_iter=1000)
