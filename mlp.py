d525b27ec758ffd5104684838426881bedf05edb
aya1151 <105955636+aya1151@users.noreply.github.com>
2025-04-22 18:06:54 -0400

    Create mlp.py

---
+++ mlp.py
@@ -0,0 +1,16 @@
+from sklearn.neural_network import MLPClassifier
+from sklearn.datasets import make_classification
+from sklearn.model_selection import train_test_split
+
+X, y = make_classification(n_samples=100, random_state=1)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# might need to play around with the hidden layer and max_iter hyperparameter to get better results
+clf = MLPClassifier(hidden_layer_sizes=(128,), activation='relu', max_iter=1000)
+clf.fit(X_train, y_train)
+y_pred = clf.predict(X_test)
+from sklearn.metrics import accuracy_score
+acc = accuracy_score(y_test, y_pred)
+print(acc)
+
+# I tested with 2000 max_iter and accuracy drops from 0.92 to 0.88. Max_iter = 700 makes accuracy score drop from 0.92 to 0.84. 
