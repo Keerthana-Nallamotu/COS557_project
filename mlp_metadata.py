b66152b90ea1f9b5d65f6aea8e6cbc53956535bc
aya1151 <105955636+aya1151@users.noreply.github.com>
2025-05-04 08:53:52 -0400

    Update mlp_metadata.py

--- mlp_metadata.py
+++ mlp_metadata.py
@@ -2,6 +2,16 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.datasets import make_classification
 from sklearn.model_selection import train_test_split
 import numpy as np
+import os
+import torch
+
+
+# Set up device
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu") #ideally would use gpu but given the project guidelines, will use cpu here
+print("Using device:", device)
+
+# Define paths
+metadata = "/data/home/cos557/data/rothman/parsed_xray_files_log.csv"
 
 #X, y = make_classification(n_samples=100, random_state=1)
 #X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
